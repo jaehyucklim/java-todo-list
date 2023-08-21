d00a42dfbd0aceedea0c99ff1862f9bb91a1d044
jaehyucklim <jhlim087@gmail.com>
2023-08-21 19:33:32 +0900

    hw1

--- src/main/java/org/homework/App.java
+++ src/main/java/org/homework/App.java
@@ -1,3 +1,5 @@
+package org.homework;
+
 import java.util.Scanner;
 
 public class App {
--- src/main/java/org/homework/TodoList.java
+++ src/main/java/org/homework/TodoList.java
@@ -1,3 +1,5 @@
+package org.homework;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
